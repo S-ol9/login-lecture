0f6bfc128318def71bd46dc3d53ef013ef8ac782
S-ol9 <n.solee9@gmail.com>
2023-07-27 12:37:43 +0900

    돔을 이용하여 HTML 객체 저장

--- src/public/js/home/login.js
+++ src/public/js/home/login.js
@@ -1,4 +1,17 @@
 "user strict";
 
-console.log("Hello")
-console.log("bye")
+const id = document.querySelector("#id"), // 태그명이랑 속성 값이 같으면 안 되므로 앞에 # 추가
+    psword = document.querySelector("#psword"), // 그냥 쓰면 태그면 속성 값은 # 붙여서
+    loginBtn = document.querySelector("button"); // button은 태그 그대로
+
+loginBtn.addEventListener("click", login);
+
+function login() {
+    const req = { // 오브젝트는 값을 넣어줄 때 키, 값 형태로
+        id: id.value,
+        psword: psword.value,
+    };
+
+    console.log(req);
+
+};
