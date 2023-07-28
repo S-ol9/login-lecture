e9c26df20c5b43d79162a2b5868df6e1536a5e4f
S-ol9 <n.solee9@gmail.com>
2023-07-28 14:28:52 +0900

    회원가입 페이지 구현

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -6,5 +6,6 @@ const ctrl = require("./home.ctrl")
 
 router.get("/", ctrl.output.hello);
 router.get("/login", ctrl.output.login);
+router.get("/register", ctrl.output.register);
 router.post("/login", ctrl.process.login);
 module.exports = router;
